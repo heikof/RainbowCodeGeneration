1ea75dc44bb59649f9bbdc65117df67186d9e7a6
Heiko Franz <heikosebastian.franz@gmail.com>
2017-11-23 18:33:53 +0100

    Nuspec / versioning cleanup

--- src/RainbowCodeGeneration/Properties/AssemblyInfo.cs
+++ src/RainbowCodeGeneration/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RainbowCodeGeneration")]
-[assembly: AssemblyCopyright("Copyright © Heiko Franz 2016")]
+[assembly: AssemblyCopyright("Copyright © Heiko Franz 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
